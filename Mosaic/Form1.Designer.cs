940ac75435565eb0151c415147825dccd57412d0
TheIPv <anymaxi@yandex.ru>
2023-10-06 21:11:09 +0300

    feat: final version

---
+++ Mosaic/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Mosaic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.TLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(43, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.button1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(433, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выйти";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mosaic.Resource1.start_logo;
+            this.pictureBox1.InitialImage = global::Mosaic.Resource1.start_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(191, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 151);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(498, 172);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 123);
+            this.button7.TabIndex = 2;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button8.Location = new System.Drawing.Point(498, 53);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 123);
+            this.button8.TabIndex = 1;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Location = new System.Drawing.Point(359, 172);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(139, 123);
+            this.button9.TabIndex = 3;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.AutoSize = true;
+            this.button10.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button10.Location = new System.Drawing.Point(359, 53);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(139, 123);
+            this.button10.TabIndex = 0;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Visible = false;
+            this.button10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 53);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(280, 242);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(284, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Время:";
+            this.label1.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(266, 11);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(166, 35);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // TLabel
+            // 
+            this.TLabel.AutoSize = true;
+            this.TLabel.BackColor = System.Drawing.Color.Red;
+            this.TLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.TLabel.Location = new System.Drawing.Point(356, 18);
+            this.TLabel.Name = "TLabel";
+            this.TLabel.Size = new System.Drawing.Size(0, 18);
+            this.TLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(693, 453);
+            this.Controls.Add(this.TLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Mosaic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label TLabel;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ Mosaic/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Mosaic
+{
+    public partial class Form1 : Form
+    {
+        int count = 600000;
+        private Button selected = null;
+        Bitmap picture = null;
+        private Bitmap btn7 = null;
+        private Bitmap btn8 = null;
+        private Bitmap btn9 = null;
+        private Bitmap btn10 = null;
+
+        private Button btnFromIndex(int index) // Получаем объект кнопки по id
+        {
+            switch (index)
+            {
+                case 0: return button10;
+                case 1: return button8;
+                case 2: return button7;
+                case 3: return button9;
+            }
+            return null;
+        }
+
+        private void reload() // подготавливаем программу к новому запуску
+        {
+            button7.BackgroundImage = null;
+            button8.BackgroundImage = null;
+            button9.BackgroundImage = null;
+            button10.BackgroundImage = null;
+
+            button1.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
+
+            pictureBox2.BackgroundImage = null;
+
+            timer1.Enabled = false;
+            timer1.Stop();
+            timer1.Tick -= new EventHandler(timer1_Tick);
+            count = 600000;
+
+            label1.Visible = false;
+            pictureBox3.Visible = false;
+            TLabel.Visible = false;
+        }
+
+        private void randomizeParts() // перемешиваем части мозайки
+        {
+            Random rand = new Random();
+            Button tbtn1 = null, tbtn2 = null;
+            Image timg = null;
+            int i = 0;
+            while (i < 6 || button7.BackgroundImage == btn7)
+            {
+                tbtn1 = btnFromIndex(rand.Next(0, 3));
+                timg = tbtn1.BackgroundImage;
+                tbtn2 = btnFromIndex(rand.Next(0, 3));
+                tbtn1.BackgroundImage = tbtn2.BackgroundImage;
+                tbtn2.BackgroundImage = timg;
+                ++i;
+            }
+        }
+
+        private bool checkFinish() // проверяем, верно ли собрана мозайка
+        {
+            if (button7.BackgroundImage == btn7 &&
+                button8.BackgroundImage == btn8 &&
+                button9.BackgroundImage == btn9 &&
+                button10.BackgroundImage == btn10) return true;
+            else return false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // обработчик кнопки "загрузить изображение"
+        {
+            OpenFileDialog imDiag = new OpenFileDialog();
+            try
+            {
+                if (imDiag.ShowDialog() == DialogResult.OK)
+                {
+                    picture = new Bitmap(imDiag.FileName);
+                }
+                pictureBox2.Image = new Bitmap(picture, new Size(pictureBox2.Width, pictureBox2.Height));
+                //делим изображение на 4 части
+                int w = picture.Width;
+                int h = picture.Height;
+                btn10 = new Bitmap(picture.Clone(new RectangleF(0, 0, w / 2, h / 2),
+                        picture.PixelFormat), new Size(button7.Width, button7.Height));
+                btn9 = new Bitmap(picture.Clone(new RectangleF(0, h / 2, w / 2, h / 2),
+                        picture.PixelFormat), new Size(button7.Width, button7.Height));
+                btn8 = new Bitmap(picture.Clone(new RectangleF(w / 2, 0, w / 2, h / 2),
+                        picture.PixelFormat), new Size(button7.Width, button7.Height));
+                btn7 = new Bitmap(picture.Clone(new RectangleF(w / 2, h / 2, w / 2, h / 2),
+                        picture.PixelFormat), new Size(button7.Width, button7.Height));
+
+                button1.Visible = false;
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+                button7.Visible = true;
+                button8.Visible = true;
+                button9.Visible = true;
+                button10.Visible = true;
+
+                button7.BackgroundImage = btn7;
+                button8.BackgroundImage = btn8;
+                button9.BackgroundImage = btn9;
+                button10.BackgroundImage = btn10;
+
+                randomizeParts();
+
+                //активируем таймер
+                timer1.Enabled = true;
+                timer1.Start();
+                timer1.Tick += new EventHandler(timer1_Tick);
+                timer1.Interval = 1;
+             
+                label1.Visible = true;
+                pictureBox3.Visible = true;
+                TLabel.Visible = true;
+            }
+            catch
+            {
+                MessageBox.Show("Вы выбрали некорректный файл. Попробуйте ещё раз");
+                reload();
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e) //обработчик нажатия на кнопку мозайки
+        {
+            Button b = (Button)sender;
+            Image timg = null;
+            if (selected == null)
+                selected = b;
+            else
+            {
+                timg = selected.BackgroundImage;
+                selected.BackgroundImage = b.BackgroundImage;
+                b.BackgroundImage = timg;
+                selected = null;
+                if (checkFinish())
+                {
+                    reload();
+                    MessageBox.Show("Всё правильно, с победой!");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // если прошло > 10 минут (проигрыш)
+        {
+            count -= 1000;
+            if (count != 0 && count > 0)
+            {
+                TLabel.Text = count / 60000 + ":" + ((count % 60000) >= 10000 ? (count % 60000) : 0)/1000;
+            }
+            else
+            {
+                reload();
+                MessageBox.Show("Вы проиграли");
+            }
+            timer1.Interval = 1000;
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e) // обработчик DragDrop (аналогичен обработчику кнопке 
+                                                                    // "загрузить изображение"
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                try
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    picture = new Bitmap(Image.FromFile(files[0]));
+                    pictureBox2.Image = new Bitmap(picture, new Size(pictureBox2.Width, pictureBox2.Height));
+                    int w = picture.Width;
+                    int h = picture.Height;
+                    btn10 = new Bitmap(picture.Clone(new RectangleF(0, 0, w / 2, h / 2),
+                            picture.PixelFormat), new Size(button7.Width, button7.Height));
+                    btn9 = new Bitmap(picture.Clone(new RectangleF(0, h / 2, w / 2, h / 2),
+                            picture.PixelFormat), new Size(button7.Width, button7.Height));
+                    btn8 = new Bitmap(picture.Clone(new RectangleF(w / 2, 0, w / 2, h / 2),
+                            picture.PixelFormat), new Size(button7.Width, button7.Height));
+                    btn7 = new Bitmap(picture.Clone(new RectangleF(w / 2, h / 2, w / 2, h / 2),
+                            picture.PixelFormat), new Size(button7.Width, button7.Height));
+
+                    button1.Visible = false;
+                    pictureBox1.Visible = false;
+                    pictureBox2.Visible = true;
+                    button7.Visible = true;
+                    button8.Visible = true;
+                    button9.Visible = true;
+                    button10.Visible = true;
+
+                    button7.BackgroundImage = btn7;
+                    button8.BackgroundImage = btn8;
+                    button9.BackgroundImage = btn9;
+                    button10.BackgroundImage = btn10;
+
+                    randomizeParts();
+
+                    timer1.Enabled = true;
+                    timer1.Start();
+                    timer1.Tick += new EventHandler(timer1_Tick);
+                    timer1.Interval = 1;
+
+                    label1.Visible = true;
+                    pictureBox3.Visible = true;
+                    TLabel.Visible = true;
+                }
+                catch
+                {
+                    MessageBox.Show("Вы выбрали некорректный файл. Попробуйте ещё раз");
+                    reload();
+                }
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
